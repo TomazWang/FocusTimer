82ff51546b31c9cecb31e36cd849c9758a8018d8
Tomaz Wang@VAIO <TomazWang.tw@gmail.com>
2014-12-22 19:58:25 +0800

    added get sececond function in Sections calss

--- src/com/tomaz/focustimer/other/Sections.java
+++ src/com/tomaz/focustimer/other/Sections.java
@@ -1,5 +1,14 @@
 package com.tomaz.focustimer.other;
 
 public enum Sections {
-	WORKING,LONG_REST,SHORT_REST;
+	WORKING(25*60),LONG_REST(15*60),SHORT_REST(5*60);
+	
+	private int sec = 0;
+	private Sections(int sec){
+		this.sec = sec;
+	}
+	
+	public int getSec(){
+		return this.sec;
+	}
 }
