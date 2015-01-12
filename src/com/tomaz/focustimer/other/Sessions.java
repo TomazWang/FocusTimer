61ee02140a4ad2e5404f385d5c0a4906b15107f5
Tomaz Wang <TomazWang.tw@gmail.com>
2015-01-12 16:13:56 +0800

    setting short time session for testing

--- src/com/tomaz/focustimer/other/Sessions.java
+++ src/com/tomaz/focustimer/other/Sessions.java
@@ -1,7 +1,9 @@
 package com.tomaz.focustimer.other;
 
 public enum Sessions {
-	WORKING(25*60),LONG_REST(15*60),SHORT_REST(5*60);
+	
+	// TODO setting secToCount
+	WORKING(30),LONG_REST(5),SHORT_REST(15);
 	
 	private int sec = 0;
 	private Sessions(int sec){
